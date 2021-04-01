2a11e78d7bf4e8be1ebe6be5b908c8fcce09e631
Kethiannne <natedadc@gmail.com>
2021-04-01 15:29:50 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles/index.css";
+let someStr = "I coded. I saved. I bundled.";
+console.log("Hello World!");
+const numbers = [2, 3, 5];
+
+// Arrow function. How will Internet Explorer cope with it?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
